58af8cddb2c165ecca75e6e01dde4a80191d0221
CNhumanhope <68433914+CNhumanhope@users.noreply.github.com>
2020-07-19 00:39:57 +0800

    Score(while)
    while语句实现评委评分

---
+++ Score(while).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,n;
+	double score,sum,avg;
+	sum=0;
+	cout<<"输入评委个数"<<endl;
+	cin>>n;
+	cout<<"输入n个评委的评分"<<endl;
+	i=1;
+	while(i<=n)
+	{
+		cin>>score;
+		sum+=score;
+		i++;
+	}
+	avg=sum/n;
+	cout<<"选手最终得分为："<<avg<<endl;
+	system("pause");
+	return 0;
+}
